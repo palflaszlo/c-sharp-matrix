00e118a44a506c461c44f38f93226defcda39b93
László Pálfalvi <palflaszlo20@outlook.hu>
2019-10-28 21:19:10 +0100

    Add project files.

---
+++ ConsoleApp24_matrix/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24_matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. feladat
+            int n;
+            int m;
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
