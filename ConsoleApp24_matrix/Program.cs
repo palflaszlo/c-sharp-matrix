8d0a8bf845f8c0dc7e649a1613e4614f8d4cbbd6
László Pálfalvi <palflaszlo20@outlook.hu>
2019-11-13 00:27:35 +0100

    sikeresen teljesítve a teljes első feladat

--- ConsoleApp24_matrix/Program.cs
+++ ConsoleApp24_matrix/Program.cs
@@ -32,7 +32,9 @@ namespace ConsoleApp24_matrix
             {
                 Console.WriteLine("{0}: {1}", i, alsoPont[i]);
             }
+
             Console.WriteLine();
+            
             for (int i = 0; i < resztvevok; i++)
             {
                 for (int j = 0; j < kategoriak; j++)
@@ -40,7 +42,24 @@ namespace ConsoleApp24_matrix
                     kutyaSzepsegVerseny[i, j] = r.Next(0,maxPont[j]);
                 }
             }
-
+            /*
+            kutyaSzepsegVerseny[0, 0] = 40;
+            kutyaSzepsegVerseny[0, 1] = 40;
+            kutyaSzepsegVerseny[0, 2] = 0;
+            kutyaSzepsegVerseny[0, 3] = 40;
+            kutyaSzepsegVerseny[1, 0] = 50;
+            kutyaSzepsegVerseny[1, 1] = 50;
+            kutyaSzepsegVerseny[1, 2] = 50;
+            kutyaSzepsegVerseny[1, 3] = 50;
+            kutyaSzepsegVerseny[2, 0] = 29;
+            kutyaSzepsegVerseny[2, 1] = 29;
+            kutyaSzepsegVerseny[2, 2] = 29;
+            kutyaSzepsegVerseny[2, 3] = 40;
+            kutyaSzepsegVerseny[3, 0] = 40;
+            kutyaSzepsegVerseny[3, 1] = 40;
+            kutyaSzepsegVerseny[3, 2] = 40;
+            kutyaSzepsegVerseny[3, 3] = 40;
+            */
             for (int i = 0; i < resztvevok; i++)
             {
                 for (int j = 0; j < kategoriak; j++)
@@ -49,11 +68,18 @@ namespace ConsoleApp24_matrix
                 }
                 Console.WriteLine();
             }
-            //döntsük melyek estek ki
+
+            Console.WriteLine();
+
+            //döntsük melyek estek ki és 
+            //döntsük el van-e olyan aki minden kategóriában kiesett
             int kiesett = -1;
             int[] kiesettek = new int[resztvevok];
+            int kiesesekSzama = 0;
+            
             for (int i = 0; i < resztvevok; i++)
             {
+                kiesesekSzama = 0;
                 for (int j = 0; j < kategoriak; j++)
                 {
                     if(kiesett != i)
@@ -64,6 +90,14 @@ namespace ConsoleApp24_matrix
                             kiesettek[i] = i;
                         }
                     }
+                    if (kutyaSzepsegVerseny[i, j] <= alsoPont[j])
+                    {
+                        kiesesekSzama++;
+                    }
+                }
+                if (kiesesekSzama == kategoriak)
+                {
+                    Console.WriteLine("{0}. résztvevő vesztett minden kategóriában.", i);
                 }
                 if (kiesett == i)
                 {
@@ -75,23 +109,53 @@ namespace ConsoleApp24_matrix
                     Console.WriteLine("Bent maradt: {0}", i);
                 }
             }
-            for (int i = 0; i < resztvevok; i++)
+            //döntsük el van-e olyan kategória, ahol nem esett ki senki
+
+            Console.WriteLine();
+
+            int nemestekkiKategoria = 0;
+            for (int i = 0; i < kategoriak; i++)
             {
-                //Console.Write("{0} :",i);
-                for (int j = 0; j < kategoriak; j++)
+                nemestekkiKategoria = 0;
+                for (int j = 0; j < resztvevok; j++)
                 {
-                    if (kiesettek[i] == 0)
+                    if (kutyaSzepsegVerseny[j, i] > alsoPont[i])
                     {
-                        bentMaradtak[i, j] = kutyaSzepsegVerseny[i, j];
-                        //Console.Write("{0} ({1}), ", j, bentMaradtak[i,j]);
+                        nemestekkiKategoria++;
                     }
                 }
-                //Console.WriteLine();
+                if(nemestekkiKategoria == resztvevok)
+                {
+                    Console.WriteLine("A {0}. kategóriában nem volt kieső kutya.", i);
+                }
             }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < resztvevok; i++)
+            {
+                if (kiesettek[i] == 0)
+                {
+                    //Console.Write("{0} :",i);
+                    for (int j = 0; j < kategoriak; j++)
+                    {
+                        
+                        bentMaradtak[i, j] = kutyaSzepsegVerseny[i, j];
+                        Console.Write("({0}, {1}): {2};   ", i, j, bentMaradtak[i,j]);
+
+                    }
+                   
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine();
+
             //döntsük el van-e döntetlen
             int[] legjobb = new int[resztvevok];
+            
             int legeslegjobb = 0;
-            //int hanyszorNyert = 0;
+            int hanyszorNyert = 0;
             int[] kinyert = new int[kategoriak];
             int dontetlenEgy = 0;
             int dontetlenKetto = 0;
@@ -107,15 +171,19 @@ namespace ConsoleApp24_matrix
                     {
                         legeslegjobb = bentMaradtak[i, j];
                         dontetlenEgy = i;
-                        //hanyszorNyert++;
                         kinyert[j] = i;
+                        
                     }
                     else if(legeslegjobb == bentMaradtak[i, j])
                     {
                         dontetlenKetto = i;
                     }
+                    
+                    
                 }
-                if(dontetlenEgy != dontetlenKetto && dontetlenKetto != -1)
+                if (dontetlenEgy != dontetlenKetto && 
+                   dontetlenKetto > dontetlenEgy &&
+                   dontetlenKetto != -1)
                 {
                     Console.WriteLine("Itt van egy holtverseny az első helyért: a {0} és a {1} " +
                         "versenyző között. Ebben a kategóriában: {2}", dontetlenEgy, dontetlenKetto, j);
@@ -123,30 +191,106 @@ namespace ConsoleApp24_matrix
                 }
                 else
                 {
-                    Console.WriteLine("Nincsen döntetlen.");
+                    Console.WriteLine("Nincsen döntetlenaz első helyért.");
                 }
-                //legjobb[j]++;
             }
-            //döntsük el van-e olyan aki minden kategóriában nyert
-            int gyoztes = kinyert[0];
-            int gyozelmekSzama = 0;
             
+
+
+
+
+            
+            Console.WriteLine();
+
+            //döntsük el, hogy van –e olyan, aki abszolút győztes (összpontszám), de minden kategóriában volt nála jobb!
+            //Console.WriteLine("Anyád");
+            int[] legjobbEredmenyek = new int[kategoriak];
+            legeslegjobb = 0;
+            for (int j = 0; j < kategoriak; j++)
+            {
+                legeslegjobb = 0;
+                for (int i = 0; i < resztvevok; i++)
+                {
+                    if (legeslegjobb < kutyaSzepsegVerseny[i, j])
+                    {
+                        legeslegjobb = kutyaSzepsegVerseny[i, j];
+                        legjobbEredmenyek[j] = kutyaSzepsegVerseny[i, j];
+
+                    }
+                }
+
+                Console.WriteLine("itt " + legjobbEredmenyek[j]);
+            }
+
+
+            int[] osszpontszam = new int[resztvevok];
+            for (int i = 0; i < resztvevok; i++)
+            {
+                for (int j = 0; j < kategoriak; j++)
+                {
+                    osszpontszam[i] += kutyaSzepsegVerseny[i, j];
+                }
+                Console.WriteLine("{0}. résztvevő osszpontszáma: {1}", i, osszpontszam[i]);
+            }
+
+            Console.WriteLine();
+
+            int abszolutGyoztes = osszpontszam[0];
+            int abszolutNyertes = 0;
+            for (int i = 0; i < resztvevok; i++)
+            {
+                if (abszolutGyoztes < osszpontszam[i])
+                {
+                    //Console.WriteLine("Anyád" + i);
+                    abszolutGyoztes = osszpontszam[i];
+                    abszolutNyertes = i;
+                }
+            }
+            Console.WriteLine("{0}. résztvevő az abszolút győztes (összpontszám)", abszolutNyertes);
+            int nalajobb = 0;
+            for (int j = 0; j < kategoriak; j++)
+            {
+                if (kutyaSzepsegVerseny[abszolutNyertes, j] < legjobbEredmenyek[j])
+                {
+                    nalajobb++;
+                }
+            }
+            if (nalajobb == kategoriak)
+            {
+                Console.WriteLine("{0}. résztvevő az abszolút győztes (összpontszám), de minden kategóriában volt nála jobb!", abszolutNyertes);
+            }
+
+
+            //döntsük el van-e olyan aki több kategóriában nyert
             for (int i = 0; i < kinyert.Length; i++)
             {
-                if(gyoztes == kinyert[i])
+                Console.WriteLine(kinyert[i]);
+            }
+            hanyszorNyert = 0;
+            for (int i = 0; i < resztvevok; i++)
+            {
+                hanyszorNyert = 0;
+                for (int j = 0; j < kinyert.Length; j++)
                 {
-                    gyozelmekSzama++;
+                    if (i == kinyert[j])
+                    {
+                        hanyszorNyert++;
+                    }
+                }
+                if (hanyszorNyert < kategoriak && hanyszorNyert>0)
+                {
+                    Console.WriteLine("{0}: nyert {1}-szer", i, hanyszorNyert);
+                }
+
+                Console.WriteLine();
+
+                //döntsük el van-e olyan aki minden kategóriában nyert
+                if (hanyszorNyert == kategoriak && dontetlen == false)
+                {
+                    Console.WriteLine("{0}. résztvevő nyert minden kategóriában.", kinyert[i]);
                 }
             }
-            if (gyozelmekSzama == kategoriak && dontetlen == false)
-            {
-                Console.WriteLine("{0}. résztvevő nyert minden kategóriában.", kinyert[0]);
-            }
-            else
-            {
-                Console.WriteLine("Nincsen olyan, aki minden kategóriában a legjobb.");
-            }
-            
+         
             Console.ReadLine();
         }
     }
